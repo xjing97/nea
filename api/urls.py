12d155c272163645151fa7115f8c8cbf2a1a20b6
WZR <zhiren@aviationvirtual.com.sg>
2021-01-05 11:20:36 +0800

    Added api for change password

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 
 from result.views import store_result
-from .views import login
+from .views import login, ChangePasswordView
 
-app_name = 'core'
+app_name = 'api'
 
 # FOR UNITY SIDE ONLY
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('change_password/', ChangePasswordView.as_view(), name='auth_change_password'),
     path('store-result/', store_result, name='storeResult'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from api.serializers import ChangePasswordSerializer
+from core.models import User
 from core.serializers import LoginSerializer
 from nea.decorators import permission_exempt
+
+from django.shortcuts import get_object_or_404
 
 
 @api_view(['POST'])
@@ -16,3 +22,14 @@ def login(request):
         return Response(status=400, data=form.errors)
     res = form.login()
     return res
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset, id=self.request.user.id)
+        return obj
