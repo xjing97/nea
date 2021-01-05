29cc8bb3cd1c4031c6dfab297e3ff8911fd44771
WZR <zhiren@aviationvirtual.com.sg>
2021-01-05 15:07:13 +0800

    Enhanced to get user_can_attend

--- config/views.py
+++ config/views.py
@@ -1,4 +1,4 @@
-from django.db.models import F, Case, When, Value, CharField
+from django.db.models import F, Case, When, Value, CharField, Count, IntegerField, BooleanField
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,15 +7,33 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from config.models import Config
+from result.models import Result
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_config_by_macid(request):
+    user_id = request.user.id
     mac_id = request.GET.get('mac_id', '')
     if not mac_id:
         return Response(status=400, data={'message': 'mac_id is required'})
 
+    # check quiz can be retake or not and user's attempt
+    attended_modules = Result.objects.filter(
+        user__id=user_id
+    ).values(
+        'scenario__module__id'
+    ).annotate(
+        attempt=Count('scenario__module__id')
+    )
+
+    attended_dict = {}
+    for module in list(attended_modules):
+        attended_dict[module['scenario__module__id']] = module['attempt']
+
+    whens = [
+        When(scenario__module__id=k, then=v) for k, v in attended_dict.items()
+    ]
     config = Config.objects.filter(
         mac_ids__contains=mac_id
     ).annotate(
@@ -25,9 +43,18 @@ def get_config_by_macid(request):
         cover_photo=F('scenario__cover_image'),
         description=F('scenario__description'),
         level=F('scenario__level'),
-        scenario_title=F('scenario__scenario_title')
+        scenario_title=F('scenario__scenario_title'),
+        passing_score=F('scenario__module__passing_score'),
+        quiz_attempt=F('scenario__module__quiz_attempt'),
+        user_attempt=Case(*whens, default=0, output_field=IntegerField()),
+    ).annotate(
+        user_can_attend=Case(
+            When(user_attempt__lt=F('scenario__module__quiz_attempt'), then=Value(True)),
+            default=Value(False), output_field=BooleanField()
+        ),
     ).values(
-        'config', 'module_name', 'building_type', 'cover_photo', 'description', 'level', 'scenario_title'
+        'id', 'config', 'module_name', 'building_type', 'cover_photo', 'description', 'level', 'scenario_title',
+        'passing_score', 'user_can_attend', 'quiz_attempt', 'user_attempt'
     )
 
     return Response(data={'data': list(config)})
