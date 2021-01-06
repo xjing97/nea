a45fa0797475713a2fdabcf1a052ebf1e281c3f3
vasanta <vasanta>
2021-01-06 18:39:20 +0800

    Updated admin.py

--- result/admin.py
+++ result/admin.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Result
 
+
 class ResultAdmin(admin.ModelAdmin):
-    list_display = ('scenario', 'user', 'time_spend', 'results','is_pass','dateCreated', 'dateUpdated')
-    list_filter =('scenario', 'user', 'time_spend', 'results','is_pass','dateCreated', 'dateUpdated')
+    list_display = ('scenario', 'user', 'time_spend', 'results', 'is_pass', 'dateCreated', 'dateUpdated')
+    list_filter = ('scenario', 'user', 'time_spend', 'results', 'is_pass', 'dateCreated', 'dateUpdated')
     search_fields = ('scenario',)
     ordering = ('scenario',)
     filter_horizontal = ()
+
 
 admin.site.register(Result, ResultAdmin)
