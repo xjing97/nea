d9e4f568de76bdbd9f5bd31e86caf07193ae14d5
WZR <zhiren@aviationvirtual.com.sg>
2021-01-11 14:48:44 +0800

    Refactored code

--- config/models.py
+++ config/models.py
@@ -1,12 +1,53 @@
 import json
 
 from django.db import models
+from django.db.models import Count, When, F, Case, Value, CharField, IntegerField, BooleanField
 
 from module.models import Scenario
+from result.models import Result
 
 
 class ConfigManager(models.Manager):
-    pass
+    def get_config_by_macid(self, user_id, mac_id):
+        # check quiz can be retake or not and user's attempt
+        attended_modules = Result.objects.filter(
+            user__id=user_id
+        ).values(
+            'scenario__module__id'
+        ).annotate(
+            attempt=Count('scenario__module__id')
+        )
+
+        attended_dict = {}
+        for module in list(attended_modules):
+            attended_dict[module['scenario__module__id']] = module['attempt']
+
+        whens = [
+            When(scenario__module__id=k, then=v) for k, v in attended_dict.items()
+        ]
+        config = Config.objects.filter(
+            mac_ids__contains=mac_id
+        ).annotate(
+            module_name=F('scenario__module__module_name'),
+            building_type=Case(When(scenario__high_rise=True, then=Value('High rise')),
+                               default=Value('Low rise'), output_field=CharField()),
+            cover_photo=F('scenario__cover_image'),
+            description=F('scenario__description'),
+            level=F('scenario__level'),
+            scenario_title=F('scenario__scenario_title'),
+            passing_score=F('scenario__module__passing_score'),
+            quiz_attempt=F('scenario__module__quiz_attempt'),
+            user_attempt=Case(*whens, default=0, output_field=IntegerField()),
+        ).annotate(
+            user_can_attend=Case(
+                When(user_attempt__lt=F('scenario__module__quiz_attempt'), then=Value(True)),
+                default=Value(False), output_field=BooleanField()
+            ),
+        ).values(
+            'id', 'config', 'module_name', 'building_type', 'cover_photo', 'description', 'level', 'scenario_title',
+            'passing_score', 'user_can_attend', 'quiz_attempt', 'user_attempt'
+        )
+        return config
 
 
 class Config(models.Model):
@@ -20,3 +61,5 @@ class Config(models.Model):
     mac_ids = models.TextField(default=json.dumps([]))
     dateCreated = models.DateTimeField(auto_now_add=True)
     dateUpdated = models.DateTimeField(auto_now=True)
+
+    objects = ConfigManager()
--- config/views.py
+++ config/views.py
@@ -1,14 +1,9 @@
-from django.db.models import F, Case, When, Value, CharField, Count, IntegerField, BooleanField
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from config.models import Config
 from core.models import User
-from result.models import Result
 
 
 @api_view(['GET'])
@@ -22,43 +17,6 @@ def get_config_by_macid(request):
         if not mac_id:
             return Response(status=400, data={'message': 'mac_id is required'})
 
-    # check quiz can be retake or not and user's attempt
-    attended_modules = Result.objects.filter(
-        user__id=user_id
-    ).values(
-        'scenario__module__id'
-    ).annotate(
-        attempt=Count('scenario__module__id')
-    )
-
-    attended_dict = {}
-    for module in list(attended_modules):
-        attended_dict[module['scenario__module__id']] = module['attempt']
-
-    whens = [
-        When(scenario__module__id=k, then=v) for k, v in attended_dict.items()
-    ]
-    config = Config.objects.filter(
-        mac_ids__contains=mac_id
-    ).annotate(
-        module_name=F('scenario__module__module_name'),
-        building_type=Case(When(scenario__high_rise=True, then=Value('High rise')),
-                           default=Value('Low rise'), output_field=CharField()),
-        cover_photo=F('scenario__cover_image'),
-        description=F('scenario__description'),
-        level=F('scenario__level'),
-        scenario_title=F('scenario__scenario_title'),
-        passing_score=F('scenario__module__passing_score'),
-        quiz_attempt=F('scenario__module__quiz_attempt'),
-        user_attempt=Case(*whens, default=0, output_field=IntegerField()),
-    ).annotate(
-        user_can_attend=Case(
-            When(user_attempt__lt=F('scenario__module__quiz_attempt'), then=Value(True)),
-            default=Value(False), output_field=BooleanField()
-        ),
-    ).values(
-        'id', 'config', 'module_name', 'building_type', 'cover_photo', 'description', 'level', 'scenario_title',
-        'passing_score', 'user_can_attend', 'quiz_attempt', 'user_attempt'
-    )
+    config = Config.objects.get_config_by_macid(user_id, mac_id)
 
     return Response(data={'data': list(config)})
