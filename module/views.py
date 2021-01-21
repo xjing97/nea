041b62ce25c659431c7fc32b3ba83db35937cc5b
vasanta <vasanta>
2021-01-21 15:54:42 +0800

    Added permission handling for module views

--- module/views.py
+++ module/views.py
@@ -6,11 +6,16 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from module.models import Module
+from nea.validator import ValidateIsAdmin
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_all_modules(request):
+    validator = ValidateIsAdmin()
+    if not validator.validate(request.user.id):
+        return Response(status=403, data={'message': validator.error_message})
+
     modules = Module.objects.get_all_modules()
 
     return Response(data={'data': modules})
@@ -19,6 +24,10 @@ def get_all_modules(request):
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def count_modules_by_difficulty(request):
+    validator = ValidateIsAdmin()
+    if not validator.validate(request.user.id):
+        return Response(status=403, data={'message': validator.error_message})
+
     modules = Module.objects.count_modules_by_difficulty()
 
     return Response(data={'data': modules})
