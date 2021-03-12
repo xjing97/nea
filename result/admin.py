528588e86c367e505889015cd69711a1900ae7a3
vasanta <vasanta@aviationvirtual.com.sg>
2021-03-12 13:10:33 +0800

    Updated results admin

--- result/admin.py
+++ result/admin.py
@@ -5,10 +5,12 @@ from .models import Result
 
 
 class ResultAdmin(admin.ModelAdmin):
-    list_display = ('scenario', 'user', 'time_spend', 'results', 'is_pass', 'dateCreated', 'dateUpdated')
-    list_filter = ('scenario', 'user', 'time_spend', 'results', 'is_pass', 'dateCreated', 'dateUpdated')
-    search_fields = ('scenario',)
-    ordering = ('scenario',)
+    list_display = ('scenario', 'user', 'time_spend', 'results', 'is_pass', 'critical_failure',
+                    'dateCreated', 'dateUpdated')
+    list_filter = ('scenario', 'user', 'time_spend', 'results', 'is_pass', 'critical_failure',
+                   'dateCreated', 'dateUpdated')
+    search_fields = ('scenario__scenario_title', 'user__username', 'results', 'is_pass', 'critical_failure')
+    ordering = ('scenario__scenario_title',)
     filter_horizontal = ()
 
 
