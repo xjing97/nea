fc60fd88da4cc6ae00444c086925f3ce89a55f8b
vasanta <vasanta@aviationvirtual.com.sg>
2021-05-06 11:19:59 +0800

    Updated create_modules command to handle special non ascii char

--- module/management/commands/create_modules.py
+++ module/management/commands/create_modules.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from django.core.management import BaseCommand
 
 from module.models import Module, Scenario
+from result.models import Result
 
 
 class Command(BaseCommand):
@@ -12,10 +13,12 @@ class Command(BaseCommand):
 
         hdbScenario = Scenario.objects.create(module=module1, scenario_title='Vector Inspection (HDB)',
                                               inspection_site='HDB', cover_image='upload/scenario/hdb.png',
-                                              default_config=open('module/json/hdb.json').read())
+                                              default_config=open('module/json/hdb.json').read().encode(
+                                                  'cp1252').decode('utf-8'))
         hdbScenario.save()
 
         landedScenario = Scenario.objects.create(module=module1, scenario_title='Vector Inspection (Landed)',
                                                  inspection_site='Landed', cover_image='upload/scenario/landed.png',
-                                                 default_config=open('module/json/landed.json').read())
+                                                 default_config=open('module/json/landed.json').read().encode(
+                                                     'cp1252').decode('utf-8'))
         landedScenario.save()
